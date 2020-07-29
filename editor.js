f4d6c131b27ca49b862e2f9d3ab2f10751eb87c2
Lucas David Han <ldhan0715@gmail.com>
2020-07-29 09:03:51 +0900

    Update Function Names

--- editor.js
+++ editor.js
@@ -8,8 +8,8 @@ let keyHistory = {
     up: new Array()
 };
 
-function stringInsert(thisString,index,string,rem = 0) {
-    return thisString.slice(0,index) + string + thisString.slice(index + rem);
+String.prototype.insert = function(index,string) {
+    return this.slice(0,index) + string + this.slice(index);
 }
 
 function push(key,history) {
@@ -32,14 +32,13 @@ function moveCursor(count,cursor = editor.selectionStart) {
 function bracketAutocomplete(key) {
     function closeBracket(closer) {
         function checkMultipleBracket(closer) {
-            console.log(editor.value[cursor] + editor.value[cursor + 1]);
             if (editor.value[cursor] + editor.value[cursor + 1] === `${closer}${closer}`) {
                 editor.value = editor.value.slice(0,cursor) + editor.value.slice(cursor + 2);
                 moveCursor(0,cursor);
             }
         }
         const cursor = editor.selectionStart;
-        editor.value = stringInsert(editor.value,cursor,closer);
+        editor.value = editor.value.insert(cursor,closer);
         moveCursor(0,cursor);
         brackets.forEach((bracket) => {
             checkMultipleBracket(bracket);
@@ -66,7 +65,7 @@ function keyDownEvent(event) {
     push(key,"down");
     if (key === "Tab") {
         event.preventDefault();
-        editor.value = stringInsert(editor.value,editor.selectionStart,"\t");
+        editor.value = editor.value.insert(editor.selectionStart,"\t");
         moveCursor(-2);
     }
 }
@@ -77,9 +76,9 @@ function keyUpEvent(event) {
     bracketAutocomplete(key);
     if (keyHistory.up[0] === "{" && keyHistory.up[1] === "Enter" && !keyHistory.up.includes("Backspace")) {
         let cursor = editor.selectionStart;
-        editor.value = stringInsert(editor.value,cursor,"\n");
+        editor.value = editor.value.insert(cursor,"\n");
         cursor = editor.selectionStart;
-        editor.value = stringInsert(editor.value,cursor - 2,"\t");
+        editor.value = editor.value.insert(cursor - 2,"\t");
         moveCursor(-1,cursor);
     }
     
@@ -87,7 +86,7 @@ function keyUpEvent(event) {
     const closingIndex = editor.value.lastIndexOf("}");
 
     if (openingIndex < closingIndex && openingIndex + 3 < editor.selectionStart && editor.selectionStart < closingIndex && keyHistory.down.includes("Enter")) {
-        editor.value = stringInsert(editor.value,editor.selectionStart,"\t");
+        editor.value = editor.value.insert(editor.selectionStart,"\t");
         moveCursor(-2);
     }
 }
