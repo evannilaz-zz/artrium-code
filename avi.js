b4fb3db227a21227c3ef7689f900fc12f0c9786a
Lucas David Han <ldhan0715@gmail.com>
2020-08-01 13:02:23 +0900

    Add: Toggle Sidebar

--- avi.js
+++ avi.js
@@ -1,7 +1,22 @@
-document.querySelector("h3").addEventListener("mouseenter",() => {
-    document.querySelector("h3").querySelector("div").style.width = "100%";
+const navHeader = document.querySelector("nav h3");
+const navBtn = document.querySelector("nav button");
+
+navHeader.addEventListener("mouseenter",() => {
+    navHeader.querySelector("div").style.width = "100%";
 });
 
-document.querySelector("h3").addEventListener("mouseleave",() => {
-    document.querySelector("h3").querySelector("div").style.width = "0";
+navHeader.addEventListener("mouseleave",() => {
+    navHeader.querySelector("div").style.width = "0";
 });
+
+navBtn.addEventListener("click",() => {
+    const explorer = document.querySelector("#fileExplorer");
+    const editor = document.querySelector("#edit");
+    if (explorer.style.width === "0%") {
+        editor.style.width = "80%";
+        explorer.style.width = "20%";
+    } else {
+        explorer.style.width = "0%";
+        editor.style.width = "100%";
+    }
+})
