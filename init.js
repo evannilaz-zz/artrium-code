ed9f90db6392a5b3ad30f51ca9d3779a4e0329c6
Lucas David Han <ldhan0715@gmail.com>
2020-08-05 11:55:10 +0900

    Misguided

--- init.js
+++
@@ -1,9 +0,0 @@
-const mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
-
-function checkMobile() {
-    if (mobile.test(navigator.userAgent)) {
-        location.replace("blocked.html");
-    }
-}
-
-checkMobile();
