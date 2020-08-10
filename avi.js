f9cc6cba744deef266d07bd1debdc3f802213038
Lucas David Han <ldhan0715@gmail.com>
2020-08-10 13:16:40 +0900

    Update: Version Indicator Animation

--- avi.js
+++ avi.js
@@ -4,16 +4,16 @@ const navDownload = document.querySelector("nav #download");
 
 navHeader.addEventListener("mouseenter",() => {
     navHeader.querySelector("div").style.width = "95%";
-    navHeader.querySelector("div").style.marginLeft = "0";
     navHeader.querySelector("a>span").style.filter = "opacity(1)";
     document.querySelector("nav>span").style.filter = "opacity(1)";
 });
 
 navHeader.addEventListener("mouseleave",() => {
     navHeader.querySelector("div").style.width = "0";
-    navHeader.querySelector("div").style.marginLeft = "45%";
+    navHeader.querySelector("div").style.marginLeft = "95%";
     navHeader.querySelector("a>span").style.filter = "opacity(0)";
     document.querySelector("nav>span").style.filter = "opacity(0)";
+    setTimeout(() => {navHeader.querySelector("div").style.marginLeft = "0"},200);
 });
 
 navToggle.addEventListener("click",() => {
