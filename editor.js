2cec70a798bbed79b6600c44cbd8a07c53434b8b
Lucas David Han <ldhan0715@gmail.com>
2020-08-18 20:38:30 +0900

    Add: .gitignore

--- editor.js
+++ editor.js
@@ -84,6 +84,10 @@ function shortcutKey(event) {
     } else if (keyHistory2[keyHistory2.length - 2] === "Command" && keyHistory2[keyHistory2.length - 1] === "s") {
         event.preventDefault();
         saveFile(true);
+    } else if (keyHistory2[keyHistory2.length - 2] === "Control" && keyHistory2[keyHistory2.length - 1] === ",") {
+        location.href += "settings";
+    } else if (keyHistory2[keyHistory2.length - 2] === "Command" && keyHistory2[keyHistory2.length - 1] === ",") {
+        location.href += "settings";
     }
 }
 
